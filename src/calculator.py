d41f49f2a56306b75e5a20f94b9e45fab943f5fb
AlexFxw <alex93179@gmail.com>
2018-04-13 19:19:49 +0800

    完成了二元组的算法
    初步了基于字的二元模型的算法，然而存在一些奇怪的情况尚待优化

--- src/calculator.py
+++ src/calculator.py
@@ -1,34 +1,83 @@
 from file_loader import Analyzer
 import re
+from pypinyin import lazy_pinyin
+import math
 
 input_path = '../data/input.txt'
 output_path = '../data/output.txt'
+c_num = 20
+alpha = 0.0000000000000001
+beta = 1
 
 class Calculator:
-    def __init__(self):
+    def __init__(self,a):
         self.word = []
         self.pinyin = []
+        self.analyzer = a
     def read_in(self, path):
         with open (path, 'r') as f:
             for line in f.readlines():
                 cur_str = re.sub('\n', '', line)
                 self.pinyin.append(re.split(' ', cur_str))
-    def calculate(self, analyzer):
+    def viterbi(self):
         for py_list in self.pinyin:
-            cur_word = '' 
-            for py in py_list:
-                for item in analyzer.find_choice(py,1,1):
-                    cur_word = cur_word + item[0]
-            print(cur_word)
-            self.word.append(cur_word)
-        pass
-
-
+            l = len(py_list)
+            pbty_arr = {} 
+            for i in range(1, l):
+                py = py_list[i-1] + ' ' + py_list[i]
+                cur_list = self.analyzer.find_choice(py_list[i], 1)
+                last_list = self.analyzer.find_choice(py_list[i-1], 1)
+                pair_list = self.analyzer.find_choice(py, 2)
+                tmp_dict = {}
+                for cur in cur_list:
+                    cur = cur[0]
+                    tmp_dict[cur] = {}
+                    for last in last_list:
+                        last = last[0]
+                        tmp_dict[cur][last] = 0.0
+                        pair_cnt = self.sum(last+cur, pair_list) 
+                        p = self.trans_pbty(last,cur,last_list,cur_list,pair_list)
+                        if i == 1:
+                            tmp_dict[cur][last] = p
+                        else:
+                            for j in pbty_arr[i-1][last].keys():
+                                f = pbty_arr[i-1][last][j] * p 
+                                if f > tmp_dict[cur][last]:
+                                    tmp_dict[cur][last] = f
+                pbty_arr[i] = tmp_dict
+            k = l-1
+            ans_st = ''
+            next_w = ''
+            while k > 0:
+                f = 0.0
+                for cur in pbty_arr[k].keys():
+                    for last in pbty_arr[k][cur].keys():
+                        if pbty_arr[k][cur][last] > f:
+                            f = pbty_arr[k][cur][last]
+                            cur_w = cur
+                            next_w = last
+                ans_st = cur_w + ans_st
+                k = k-1
+            ans_st = next_w + ans_st
+            print(ans_st)
+    def sum(self, word, word_list):
+        for item in word_list:
+            if word == item[0]:
+                return item[1]
+        return 0
+    def trans_pbty(self, last, cur, last_list, cur_list, pair_list):
+        '''not finished'''
+        cur_cnt = self.sum(cur, cur_list)
+        last_cnt = self.sum(last, last_list)
+        pair_cnt = self.sum(last+cur, pair_list)
+        if last_cnt == 0 or pair_cnt == 0:
+            return alpha*cur_cnt/self.analyzer.single_num
+        else:
+            return alpha*(cur_cnt/self.analyzer.single_num) + beta*(pair_cnt / last_cnt) 
+        
 a = Analyzer()
-c = Calculator()
 a.load()
-for item in a.find_choice('bei jing', 2, 2):
-    print(item[0] + str(item[1]))
+c = Calculator(a)
 c.read_in(input_path)
-c.calculate(a)
+c.viterbi()
 
--- src/file_loader.py
+++ src/file_loader.py
@@ -7,9 +7,10 @@
 
 import json
 import re
+import sys
 from pypinyin import pinyin, lazy_pinyin
 
-src_file = '../../sina_news/2016-01.txt'
+src_file = '../../sina_news/2016-'
 test_file = '../../sina_news/test.txt'
 pinyin_lib = '../data/一二级汉字表.txt'
 single_file = '../data/single_word.txt'
@@ -25,22 +26,41 @@ class Analyzer:
         self.triple_db = {}
         self.single_num = 0
         self.double_num = 0
+        '''
         self.triple_num = 0
+        '''
     def load_data(self):
         '''将汉字之外的符号与字母用正则表达式过滤'''
         re_exp = '[^\u4e00-\u9fff]+'
-        with open(test_file, 'r') as f:
-            for line in f.readlines():
-                lineData = json.loads(line)
-                data = lineData.get('html')
-                title = lineData.get('title')
-                for s in re.split(re_exp, data):
-                    self.content.append(s)
+        for i in range(2, 12):
+            if i < 10:
+                news_file = src_file + '0' + str(i) + '.txt' 
+            else:
+                news_file = src_file + str(i) + '.txt'
+            with open(news_file, 'r') as f:
+                self.content = []
+                print(self.content)
+                for line in f.readlines():
+                    lineData = json.loads(line)
+                    data = lineData.get('html')
+                    title = lineData.get('title')
+                    for s in re.split(re_exp, data):
+                        self.content.append(s)
+                '''
+                self.analyze()
+                self.write_cnt()
+                self.write_result(single_file, self.single_db)
+                self.write_result(double_file, self.double_db)
+                '''
+                print('Finished--' + news_file)
         '''Todo...输入一二级汉字表'''
     def analyze(self):
+        k = 0
         for item in self.content:
             l = len(item)
             for i in range(0, l):
+                sys.stdout.write('is analyzing---' + str(k) + '\r')
+                k = k + 1
                 ''' 单个字出现统计 '''
                 letter = item[i]
                 l_pinyin = lazy_pinyin(letter)[0]
@@ -67,7 +87,7 @@ class Analyzer:
                         else:
                             self.double_db[x_pinyin][x] = self.double_db[x_pinyin][x] + 1 
                     self.double_num = self.double_num + 1
-                ''' 三元组出现次数统计 '''
+                ''' 三元组出现次数统计
                 if i < l-3:
                     tri = item[i:i+3]
                     tri_py = lazy_pinyin(tri)
@@ -81,9 +101,12 @@ class Analyzer:
                         else:
                             self.triple_db[tri_pinyin][tri] = self.triple_db[tri_pinyin][tri] + 1 
                     self.triple_num = self.triple_num + 1
+                '''
     def write_result(self, file_path, database):
+        print('Writing' + file_path + ': ')
         with open (file_path, 'w') as f:
             for k in database.keys():
+                sys.stdout.write('is writing---' + k + '\r')
                 result = {}
                 result['Pinyin'] = k
                 result['Word'] = {}
@@ -91,14 +114,18 @@ class Analyzer:
                     result['Word'][word] = database[k][word]
                 f.write(json.dumps(result)+'\n')
     def load_result(self, file_path, database):
+        i = 0
         with open(file_path, 'r') as f:
             for line in f.readlines():
+                sys.stdout.write(file_path + 'is loading---' + str(i) + '\r')
+                i = i + 1
                 lineData = json.loads(line)
                 py = lineData['Pinyin']
                 if py not in database.keys():
                     database[py] = {}
                 for words in lineData['Word'].keys():
                     database[py][words] = lineData['Word'][words]
+            print('\n'+file_path+' is loaded')
     def write_cnt(self):
         with open(cnt_file, 'w') as f:
             cnt_result = {}
@@ -120,13 +147,29 @@ class Analyzer:
         if dict_size < choice_num:
             return result 
         return result[0:choice_num]
-    def find_choice(self, py, lth, choice_num):
+    def find_choice(self, py, lth, choice_num = 1000000):
         if lth == 2:
-            return self.dict_sort(py,choice_num,self.double_db)
-        elif lth == 3:
             return self.dict_sort(py,choice_num,self.double_db)
         else:
             return self.dict_sort(py,choice_num,self.single_db)
+    def words_sum(self, word_list, py):
+        l = len(word_list)
+        cur_word = ''
+        for word in word_list:
+            cur_word = cur_word + word
+        print('Testing' + cur_word)
+        if l == 1:
+            if cur_word not in self.single_db[py].keys():
+                return 0
+            else:
+                return self.single_db[py][cur_word]
+        elif l == 2:
+            if cur_word not in self.double_db[py].keys():
+                return 0
+            else:
+                return self.double_db[py][cur_word]
+        else:
+            return 0
     def show_db(self):
         print(self.single_db)
         print(self.double_db)
@@ -134,22 +177,26 @@ class Analyzer:
     def load(self):
         self.load_result(single_file, self.single_db)
         self.load_result(double_file, self.double_db)
+        self.load_cnt()
+'''
         self.load_result(triple_file, self.triple_db)
+'''
 
 
 
 '''
 loader = Analyzer()
+loader.load()
+loader.load_cnt()
 loader.load_data()
-loader.analyze()
+print('done!')
+loader.load_data()
+loader.write_result(triple_file, loader.triple_db)
 loader.write_cnt()
 loader.write_result(single_file, loader.single_db)
 loader.write_result(double_file, loader.double_db)
-loader.write_result(triple_file, loader.triple_db)
 loader.load_result(single_file, loader.single_db)
 loader.load_result(double_file, loader.double_db)
 loader.load_result(triple_file, loader.triple_db)
-loader.load_cnt()
-loader.load()
 loader.show_db()
 '''
